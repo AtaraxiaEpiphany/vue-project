b01e5ddfdb8799c2af53efd38b3d9519272c3445
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-05-21 17:55:15 +0800

    layout-吸顶导航交互实现.

--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -2,10 +2,12 @@
 import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
+import HeaderFix from './components/HeaderFix.vue'
 
 </script>
 
 <template>
+    <HeaderFix />
     <LayoutNav />
     <LayoutHeader />
     <RouterView />
