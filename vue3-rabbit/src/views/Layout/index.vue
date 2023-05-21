e0aea9e9d7af43181fd0fb4ef060446788104fb0
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-05-21 17:10:52 +0800

    Import ali icons

--- vue3-rabbit/src/views/Layout/index.vue
+++ vue3-rabbit/src/views/Layout/index.vue
@@ -1,10 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>Index</div>
+    <LayoutNav />
+    <LayoutHeader />
     <RouterView />
-    <div class="test">test scss</div>
+    <LayoutFooter />
 </template>
-<style lang="scss">
-.test {
-    color: $priceColor;
-}
-</style>
