1bc8206063a7a66b788e446623815ac45b797200
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-05-21 15:59:34 +0800

    axios config

--- vue3-rabbit/src/App.vue
+++ vue3-rabbit/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
 // import HelloWorld from '@/components/HelloWorld.vue'
+import { getCategoryAPI } from './apis/testApi'
+getCategoryAPI().then(resp => {
+  console.log(`resp ==> `, resp)
+})
 </script>
 
 <template>
---
+++ vue3-rabbit/src/utils/http.js
@@ -0,0 +1,26 @@
+import axios from "axios"
+//Axios instance
+const httpInstance = axios.create({
+    baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
+    timeout: 5000
+})
+// Add a request interceptor
+httpInstance.interceptors.request.use(config => {
+    // Do something before request is sent
+    return config;
+}, error => {
+    // Do something with request error
+    return Promise.reject(error);
+})
+
+// Add a response interceptor
+httpInstance.interceptors.response.use(response => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data
+}, error => {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error)
+})
+export default httpInstance
