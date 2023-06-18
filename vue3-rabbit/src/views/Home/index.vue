8224c4a61db7a1a991cded4849ec7fd0a6693799
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-06-18 20:16:09 +0800

    使用slot实现组件复用

--- vue3-rabbit/src/views/Home/index.vue
+++ vue3-rabbit/src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,15 @@ import homeProduct from './components/HomeProduct.vue'
     </div>
     <HomeNew />
     <HomeHot />
-    <homeProduct />
+    <HomeProduct />
+    <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
+        <div>
+            新鲜好物
+        </div>
+    </HomePanel>
+    <HomePanel title="人气推荐" subtitle="人气推荐 好多商品">
+        <div>
+            人气推荐
+        </div>
+    </HomePanel>
 </template>
