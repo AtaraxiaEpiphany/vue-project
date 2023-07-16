ee6886f670717639d9bf383c9e7614fac21eda9d
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-07-16 10:23:32 +0800

    详情页面 热榜区 标题与列表的适配

--- vue3-rabbit/src/views/Detail/components/DetailHot.vue
+++ vue3-rabbit/src/views/Detail/components/DetailHot.vue
@@ -1,13 +1,22 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { fetchHotGoodsAPI } from '@/apis/detail'
+const props = defineProps({
+    hotType: {
+        type: Number,
+        default: 1,
+        validator(value) {
+            return [1, 2].includes(value)
+        }
+    }
+})
 const route = useRoute()
 const hotList = ref([])
 const getHotList = async () => {
     const resp = await fetchHotGoodsAPI({
         id: route.params.id,
-        type: 1,
+        type: props.hotType,
     })
     hotList.value = resp.result
 }
@@ -15,12 +24,18 @@ onMounted(() => {
     getHotList()
 })
 
+const TYPE_MAP = {
+    1: '24小时热榜',
+    2: '周热榜'
+}
+
+const title = computed(() => TYPE_MAP[props.hotType])
 </script>
 
 
 <template>
     <div class="goods-hot">
-        <h3>周日榜单</h3>
+        <h3>{{ title }}</h3>
         <!-- 商品区块 -->
         <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
             <img :src="item.picture" alt="" />
