b8388782773eb7ff8fc4da81dbe09e38feb13d26
AtaraxiaEpiphany <yuewu0527@qq.com>
2023-07-16 10:11:13 +0800

    详情页面 热榜区 基础组件封装

--- vue3-rabbit/src/views/Detail/index.vue
+++ vue3-rabbit/src/views/Detail/index.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import { getDetail } from '@/apis/detail'
 import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
-
+import DetailHot from './components/DetailHot.vue'
 const route = useRoute()
 const goods = ref({})
 const getGoods = async () => {
@@ -126,7 +126,8 @@ onMounted(() => {
                         </div>
                         <!-- 24热榜+专题推荐 -->
                         <div class="goods-aside">
-
+                            <DetailHot></DetailHot>
+                            <DetailHot></DetailHot>
                         </div>
                     </div>
                 </div>
